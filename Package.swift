ce34451e13b9e86a45d157aa2ec05bcbcb953964
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-06 08:41:50 +0300

    [Package] Add minimum target requirements

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "JavaScript",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+    ],
     products: [
         .library(name: "JavaScript", targets: ["JavaScript"]),
     ],
